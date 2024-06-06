1289e76cd8f2b1be30e0be28d22b3225b0f8263a
dheepan <csdheepan18@gmail.com>
2024-06-06 12:50:05 +0530

    <DN> 1.To make a secure DB connection

--- app/config/database.js
+++ app/config/database.js
@@ -1,12 +1,13 @@
 // config/database.js
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Test@12345',
-  database: 'UserDetails',
-  port: 3306
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 connection.connect(err => {
